e46a4bc80519ae9420af9e0a44a0c67bb46f6f21
jimiwesterholm <jimi.westerholm@gmail.com>
2019-06-15 11:27:07 +0300

    Update juttu.py

--- juttu.py
+++ juttu.py
@@ -5,7 +5,16 @@ Spyder Editor
 This is some nonsense.
 """
 from scipy.io import wavfile as wf
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('tableau-colorblind10')
 
 r, a = wf.read('po33juu.wav')
 print(a.shape)
 print(r)
+N = a.shape[0]
+L = N / r
+
+np.fft(a)
+print('jaa')
